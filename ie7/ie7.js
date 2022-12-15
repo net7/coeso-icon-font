be70d1f53ae68b549d72b9c95e063b1eb3402674
Duccio <ducciobreschi@gmail.com>
2022-12-15 09:35:21 +0100

    Add file icon

--- ie7/ie7.js
+++ ie7/ie7.js
@@ -13,6 +13,7 @@
 		el.innerHTML = '<span style="font-family: \'coeso-font\'">' + entity + '</span>' + html;
 	}
 	var icons = {
+		'icon-File': '&#xe94d;',
 		'icon-find-funding': '&#xe939;',
 		'icon-missing-info': '&#xe94a;',
 		'icon-spoken-languages': '&#xe94b;',
